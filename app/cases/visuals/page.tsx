6039c6b8ece4a43382d759444653a39e8ad88148
Dmitrii Vasilev <dvasilevdesign@gmail.com>
2026-07-03 17:45:36 +0300

    описание

--- app/cases/visuals/page.tsx
+++ app/cases/visuals/page.tsx
@@ -35,7 +35,6 @@ export default function VisualsPage() {
           />
         </section>
 
-        <CaseNavigation nextHref="/cases/upjob" />
         <ProjectCTA />
       </div>
     </ProjectPageShell>
